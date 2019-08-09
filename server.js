65e250f8db5cc8b7b4822a0ea68ed1a4142fa762
Sam Jackson <samuelsjackson7@gmail.com>
2019-08-09 12:24:54 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
